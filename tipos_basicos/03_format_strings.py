df817b0da219fe06f9d3a43423c608ae399aae1e
Luis Alberto Sánchez Juárez <luisalbertosanchezjuarez@gmail.com>
2023-04-11 19:31:49 -0600

    Se cambio agrego el archivo 03_format_strings

--- tipos_basicos/03_format_strings.py
+++ tipos_basicos/03_format_strings.py
@@ -1,5 +1,6 @@
 nombre = "Luis Alberto"
 apellido = 'Sanchez Juarez'
 
+# A esto se le conoce como formateo de strings, podemos poner cualquier cosa dentro de las llaves, ya sean variables u operaciones
 nombre_completo = f'{nombre} {apellido}'
 print(nombre_completo)
